92784aff13eaedc0380faa4b34c5e8e1a7c64cc7
Vee Uye <vee.uye@gmail.com>
2022-05-28 17:01:10 +0100

    add snapshot test to ForecastSummaries.test

---
+++ src/tests/components/ForecastSummaries.test.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import ForecastSummaries from "../../components/ForecastSummaries";
+
+describe("ForecastSummaries", () => {
+  const validProps = [
+    {
+      date: 1111111,
+      description: "Stub description 1",
+      icon: "stubIcon1",
+      temperature: {
+        max: 22,
+        min: 12,
+      },
+    },
+    {
+      date: 2222222,
+      description: "Stub description 2",
+      icon: "stubIcon2",
+      temperature: {
+        max: 24,
+        min: 13,
+      },
+    },
+  ];
+
+  it("renders correctly", () => {
+    const { asFragment } = render(<ForecastSummaries forecasts={validProps} />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
