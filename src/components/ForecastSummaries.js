a9fe87467e4c0de8cb5cedd18da0551f4c65edab
Vee Uye <vee.uye@gmail.com>
2022-05-28 16:49:29 +0100

    Add default props and validate prop types to <ForecastSummaries />, add key to <ForecastSummary />

--- src/components/ForecastSummaries.js
+++ src/components/ForecastSummaries.js
@@ -1,18 +1,39 @@
 import React from "react";
-// import PropTypes from "prop-types";
+import PropTypes from "prop-types";
+import ForecastSummary from "./ForecastSummary";
 
-function ForecastSummaries() {
-  return <div className="forecast-summaries" />;
+function ForecastSummaries({ forecasts }) {
+  return (
+    <div className="forecast-summaries">
+      {forecasts.map((forecast) => (
+        <ForecastSummary
+          key={forecast.date}
+          date={forecast.date}
+          description={forecast.description}
+          icon={forecast.icon}
+          temperature={forecast.temperature}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default ForecastSummaries;
 
-// ForecastSummary.propTypes = {
-//   date: PropTypes.number.isRequired,
-//   description: PropTypes.string.isRequired,
-//   icon: PropTypes.string.isRequired,
-//   temperature: PropTypes.shape({
-//     min: PropTypes.number,
-//     max: PropTypes.number,
-//   }).isRequired,
-// };
+ForecastSummaries.propTypes = {
+  forecasts: PropTypes.arrayOf(
+    PropTypes.shape({
+      date: PropTypes.number,
+      description: PropTypes.string,
+      icon: PropTypes.string,
+      temperature: PropTypes.shape({
+        min: PropTypes.number,
+        max: PropTypes.number,
+      }),
+    })
+  ),
+};
+
+ForecastSummaries.defaultProps = {
+  forecasts: { date: "1111111" },
+};
