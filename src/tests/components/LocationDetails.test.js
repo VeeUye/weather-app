654ec4ab782663805d31f81ccdcfe4d2952b24fa
Vee Uye <vee.uye@gmail.com>
2022-05-28 12:53:33 +0100

    add test file and assert rendered location details

---
+++ src/tests/components/LocationDetails.test.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import LocationDetails from "../../components/LocationDetails";
+
+describe("LocationDetail", () => {
+  it("renders the correct city and location props", () => {
+    const { getByText } = render(
+      <LocationDetails city="Manchester" country="UK" />
+    );
+
+    expect(getByText("Manchester, UK")).toBeTruthy();
+  });
+});
