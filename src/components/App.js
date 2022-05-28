dbf58edacd1a9dc2087936fd33a8a7953dd1d1ab
Vee Uye <vee.uye@gmail.com>
2022-05-28 12:38:04 +0100

    render location in browser

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,24 @@
 import "../styles/App.css";
 import React from "react";
+import { PropTypes } from "prop-types";
+import LocationDetails from "./LocationDetails";
 
-function App() {
+function App({ location }) {
+  const { city, country } = location;
   return (
-    <div className="prop">
-      <h1>Weather App</h1>
+    <div className="App">
+      <LocationDetails city={city} country={country} />
     </div>
   );
 }
 
 export default App;
+
+App.propTypes = {
+  location: PropTypes.shape({
+    city: PropTypes.string,
+    // eslint-disable-next-line prettier/prettier
+    country: PropTypes.string
+    // eslint-disable-next-line prettier/prettier
+  }).isRequired
+};
---
+++ src/components/LocationDetails.js
@@ -0,0 +1,15 @@
+/* eslint-disable prettier/prettier */
+import React from "react";
+import PropTypes from "prop-types";
+
+function LocationDetails(props) {
+  const { city, country } = props;
+  return <h1>{`${city}, ${country}`}</h1>;
+}
+
+export default LocationDetails;
+
+LocationDetails.propTypes = {
+  city: PropTypes.string.isRequired,
+  country: PropTypes.string.isRequired
+};
