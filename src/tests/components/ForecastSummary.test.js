d7fc2d18fffc2cb18ecaebe9bb601e7b88ac736d
Vee Uye <vee.uye@gmail.com>
2022-05-28 15:19:51 +0100

    render ForecastSummary component in test file and match to snapshot

--- src/tests/components/ForecastSummary.test.js
+++ src/tests/components/ForecastSummary.test.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import ForecastSummary from "../../components/ForecastSummary";
 
 describe("ForecastSummary", () => {
   const validProps = {
@@ -11,5 +12,16 @@ describe("ForecastSummary", () => {
       max: 22,
     },
   };
-  it("renders the forecast summary correctly", () => {});
+  it("renders the forecast summary correctly", () => {
+    const { asFragment } = render(
+      <ForecastSummary
+        date={validProps.date}
+        description={validProps.description}
+        icon={validProps.icon}
+        temperature={validProps.temperature}
+      />
+    );
+
+    expect(asFragment()).toMatchSnapshot();
+  });
 });
