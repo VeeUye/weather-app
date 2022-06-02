f417a768c4d0fbb146646557bb431a458c6ac912
Vee Uye <vee.uye@gmail.com>
2022-06-02 23:43:02 +0100

    fix failing prop type console errors

--- src/tests/components/ForecastSummaries.test.js
+++ src/tests/components/ForecastSummaries.test.js
@@ -3,34 +3,42 @@ import { render } from "@testing-library/react";
 import ForecastSummaries from "../../components/ForecastSummaries";
 
 describe("ForecastSummaries", () => {
-  const validProps = [
-    {
-      date: 1111111,
-      description: "Stub description 1",
-      icon: "800",
-      temperature: {
-        max: 22,
-        min: 12,
+  const validProps = {
+    forecasts: [
+      {
+        date: 1111111,
+        description: "Stub description 1",
+        icon: 800,
+        temperature: {
+          max: 22,
+          min: 12,
+        },
       },
-    },
-    {
-      date: 2222222,
-      description: "Stub description 2",
-      icon: "602",
-      temperature: {
-        max: 24,
-        min: 13,
+      {
+        date: 2222222,
+        description: "Stub description 2",
+        icon: 602,
+        temperature: {
+          max: 24,
+          min: 13,
+        },
       },
-    },
-  ];
+    ],
+    onForecastSelect: () => {},
+  };
 
   it("renders correctly", () => {
-    const { asFragment } = render(<ForecastSummaries forecasts={validProps} />);
+    const { asFragment } = render(
+      <ForecastSummaries
+        forecasts={validProps.forecasts}
+        onForecastSelect={validProps.onForecastSelect}
+      />
+    );
     expect(asFragment()).toMatchSnapshot();
   });
   it("renders the correct number of ForecastSummary instances", () => {
     const { getAllByTestId } = render(
-      <ForecastSummaries forecasts={validProps} />
+      <ForecastSummaries forecasts={validProps.forecasts} />
     );
 
     expect(getAllByTestId("forecast-summary")).toHaveLength(2);
