ef2d72007b06a7c86a2dcb7f0fb8fc799081d521
Vee Uye <vee.uye@gmail.com>
2022-06-02 11:34:22 +0100

    add state to <App />, set forecast to selectedForecast in <ForecastDetails />

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,22 @@
 import "../styles/App.css";
-import React from "react";
+import React, { useState } from "react";
 import { PropTypes } from "prop-types";
 import LocationDetails from "./LocationDetails";
 import ForecastSummaries from "./ForecastSummaries";
 import ForecastDetails from "./ForecastDetails";
 
 function App({ location, forecasts }) {
+  const [selectedDate, setSelectedDate] = useState(forecasts[0].date);
   const { city, country } = location;
+  const selectedForecast = forecasts.find(
+    (forecast) => forecast.date === selectedDate
+  );
 
   return (
     <div className="weather-app">
       <LocationDetails city={city} country={country} />
       <ForecastSummaries forecasts={forecasts} />
-      <ForecastDetails forecast={forecasts[0]} />
+      <ForecastDetails forecast={selectedForecast} />
     </div>
   );
 }
