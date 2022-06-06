6e8326541af70971ed372b546dc3c3b09b7e398a
Vee Uye <vee.uye@gmail.com>
2022-06-06 12:13:46 +0100

    Style more details button

--- src/components/ForecastSummary.js
+++ src/components/ForecastSummary.js
@@ -17,7 +17,9 @@ function ForecastSummary(props) {
       <div className="forecast-summary__temperature">{temperature.max}˚C</div>
       <div className="forecast-summary__description">{description}</div>
       <button type="button" className="button" onClick={() => onSelect(date)}>
-        <a href="#jump-link">More details...</a>
+        <a href="#jump-link" id="#link">
+          More details
+        </a>
       </button>
     </div>
   );
